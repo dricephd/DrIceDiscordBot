f870d5063e3312a26aeeb6fc3ff983fff2c6fd0f
dricephd <dricephd@gmail.com>
2015-12-21 22:39:31 -0600

    Updated Travis Tests
    Basic testing for logging in, could use more...

--- test/req-test.js
+++ test/req-test.js
@@ -3,10 +3,24 @@ var Discord = require("discord.js");
 var request = require("request");
 var fs = require('fs');
 
-//Spawn globally required classes
 var bot = new Discord.Client();
 
-//Finally, let the bot login.
+
+//when the bot is ready
+bot.on("ready", function () {
+	console.log("Bot Reported as ready");
+});
+
+//Login the bot
 bot.login(process.env.TestUser, process.env.TestPW, function(error, sentMsg) {
-	if (error != null) console.log("Login Errors: " + error);
+	if (error != null) return console.log("Login Errors: " + error);
+	console.log("Bot Has Logged In Successfully");
+});
+
+//Run some tests
+//???
+
+//Logout the bot
+bot.logout(function(error) {
+	if (error) return console.log("Logout Errors: " + error);
 });
