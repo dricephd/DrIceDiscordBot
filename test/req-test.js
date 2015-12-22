3d655486b8cca41d3e546f7412a1455f04242f14
dricephd <dricephd@gmail.com>
2015-12-21 22:31:16 -0600

    Travis Test Suite Updated

--- test/req-test.js
+++ test/req-test.js
@@ -2,3 +2,11 @@
 var Discord = require("discord.js");
 var request = require("request");
 var fs = require('fs');
+
+//Spawn globally required classes
+var bot = new Discord.Client();
+
+//Finally, let the bot login.
+bot.login(process.env.TestUser, process.env.TestPW, function(error, sentMsg) {
+	if (error != null) console.log("Login Errors: " + error);
+});
