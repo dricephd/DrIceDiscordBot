6fd10981235cc07cb65d1af6b33691486af8b2e6
dricephd <dricephd@gmail.com>
2016-01-02 18:30:20 -0600

    Updated VER to DEV-0.5.0

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 /*
 	This is DrIceBot, mostly a ping-pong bot that responds to commands.
 	Currently it is best configured when only running on one server due to the way config.json is utilized.
-	VERSION: 0.4.0
+	VERSION: DEV-0.5.0
 */
 
-const VERSION = "DEV-0.4.0";
+const VERSION = "DEV-0.5.0";
 
 // Load JSON Files
 var AuthDetails = require("./config/auth.json");
